7d164c00a0bbf9352a9aacff779c5821f0daea0f
taras-gr <tar.grytsenko@gmail.com>
2020-03-25 11:51:06 +0200

    Fixed navigation. Added bootstrap files.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using BethanysPieShop.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,6 +25,7 @@ namespace BethanysPieShop
                 try
                 {
                     var context = services.GetRequiredService<AppDbContext>();
+                    context.Database.Migrate();
                     DbInitializer.Seed(context);
                 }
                 catch (Exception)
@@ -32,8 +34,18 @@ namespace BethanysPieShop
                 }
             }
 
+            //MigrateDatabase(host);
+
             host.Run();
 
+        }
+
+        private static void MigrateDatabase(IWebHost host)
+        {
+            using(var scope = host.Services.CreateScope())
+            {
+
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
